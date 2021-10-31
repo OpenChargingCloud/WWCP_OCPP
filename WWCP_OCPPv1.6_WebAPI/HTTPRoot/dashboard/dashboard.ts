b1a80252071e787b217b06e06ba88674a603099d
Achim Friedland <achim.friedland@graphdefined.com>
2021-10-31 12:36:16 +0100

    First lines of the WebAPI

---
+++ WWCP_OCPPv1.6_WebAPI/HTTPRoot/dashboard/dashboard.ts
@@ -0,0 +1,28 @@
+﻿///<reference path="../../../../UsersAPI/UsersAPI/HTTPRoot/libs/date.format.ts" />
+
+function StartDashboard(ZoomToBoundingBox: boolean) {
+
+    HTTPGet("/tokens",
+
+        (status, response) => {
+
+            try
+            {
+
+                let list = JSON.parse(response) as any[];
+
+                
+
+            }
+            catch (exception) { }
+
+        },
+
+        (status, statusText, response) => {
+        }
+
+    );
+
+    //var refresh = setTimeout(StartDashboard, 30000);
+
+}
