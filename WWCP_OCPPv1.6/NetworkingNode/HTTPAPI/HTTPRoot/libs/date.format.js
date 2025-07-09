6950278cbf748a987ec6c1f5577660f50e68bb80
Achim Friedland <achim.friedland@graphdefined.com>
2025-07-09 18:46:55 +0200

    Fix events.ts and update project files

---
+++ WWCP_OCPPv1.6/NetworkingNode/HTTPAPI/HTTPRoot/libs/date.format.js
@@ -0,0 +1,5 @@
+// Type definitions for Date Format 1.2.3
+// Project: http://blog.stevenlevithan.com/archives/date-time-format
+// Definitions by: Rob Stutton <https://github.com/balrob>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+//# sourceMappingURL=date.format.js.map
